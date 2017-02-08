00705d654461ad02fb19bc4f3b474d7a1d075dd4
LeidyRios <k.athy2401@hotmail.com>
2017-02-08 15:45:57 -0500

    Initial commit

---
+++ app/src/main/java/com/example/win10/holamundo/HolaMUndo.java
@@ -0,0 +1,13 @@
+package com.example.win10.holamundo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HolaMUndo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hola_mundo);
+    }
+}
